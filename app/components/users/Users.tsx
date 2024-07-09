a11fc8b548deb1de9fdb0ba3a6cc4fa8dd883f5d
Aldian Fazrihady <aldian.f@ntucenterprise.sg>
2024-07-09 21:36:08 +0700

    Change mouse pointer when hovering over a user row.

--- app/components/users/Users.tsx
+++ app/components/users/Users.tsx
@@ -8,12 +8,22 @@ import {
   ListItem,
   ListItemText,
   Box,
+  CircularProgress,
+  Alert,
 } from "@mui/material";
 import AddUserDialog from './AddUserDialog';
 
 export const Users: React.FC<UsersProps> = ({ data }) => {
   const [userId, setUserId] = useState<string | undefined>(undefined);
   const { data: users, isError, isLoading, isSuccess } = useGetUsersQuery({ id: userId });
+
+  if (isLoading && !users) {
+    return <CircularProgress />;
+  }
+
+  if (isError) {
+    return <Alert severity="error">Failed to load users</Alert>;
+  }
 
   return (
     <Container maxWidth="sm">
@@ -25,7 +35,11 @@ export const Users: React.FC<UsersProps> = ({ data }) => {
       </Box>
       <List>
         {(users || data).map((user: User) => (
-          <ListItem key={user.id}>
+          <ListItem
+            key={user.id}
+            sx={{ cursor: 'pointer' }}
+            onClick={() => { /* Add your click handler logic here */ }}
+          >
             <ListItemText primary={user.name} />
           </ListItem>
         ))}
